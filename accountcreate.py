3b73d6b835f3c5811881d08ab2d59c896c495947
pablohop <71025955+pablohop@users.noreply.github.com>
2023-10-22 19:16:49 -0400

    added python code for account creation feature
    Basic python code for how account creation should work on the messaging service.

---
+++ accountcreate.py
@@ -0,0 +1,34 @@
+import csv
+
+def register_user():
+    username = input("Username: ")
+    password = input("Password: ")
+
+    if username_exists(username):
+        print("Username already taken.")
+        return
+
+    with open("users.csv", mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([username, password])
+        print("Account created.")
+
+def username_exists(username):
+    with open("users.csv", mode="r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == username:
+                return True
+    return False
+
+if __name__ == "__main__":
+    print("Sign Up!")
+    while True:
+        choice = input("Would you like to sign up?").strip().lower()
+        if choice == "yes":
+            register_user()
+        elif choice == "no":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice")
