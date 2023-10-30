6865659389d0a7acf3d1dd5184768ba2caf7f744
Shahria Abeed <shahriaabeed@gmail.com>
2023-10-29 22:24:06 -0400

    fixed accidental rename

--- db/tests/test_db.py
+++ db/tests/test_db.py
@@ -1,7 +1,7 @@
-import db.db_test as db_test
+import db.db as db
 
 
 def test_fetch_pets():
-    pets = db_test.fetch_pets()
+    pets = db.fetch_pets()
     assert isinstance(pets, dict)
     assert len(pets) > 0
