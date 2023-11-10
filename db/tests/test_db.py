ca28e765ea534824e614420319aabdb23cbdc982
Shahria Abeed <shahriaabeed@gmail.com>
2023-11-09 21:51:05 -0500

    removed unused test that is causing an error

--- db/tests/test_db.py
+++ db/tests/test_db.py
@@ -2,12 +2,6 @@ import db.db as db
 import pytest
 
 
-def test_fetch_pets():
-    pets = db.fetch_pets()
-    assert isinstance(pets, dict)
-    assert len(pets) > 0
-
-
 def test_db_connection():
     try:
         database = db.ConnectToDB()
