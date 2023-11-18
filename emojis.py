357b37b430291385a9b608981c1f2ac3f1954e02
pablohop <71025955+pablohop@users.noreply.github.com>
2023-11-18 14:58:54 -0500

    Update emojis.py
    Rewrote according to flake8 standards

--- emojis.py
+++ emojis.py
@@ -1,6 +1,5 @@
 import emoji
 
-# Define a dictionary of predefined emojis and their aliases
 emoji_dict = {
     "thumbs_up": ":thumbsup:",
     "heart_eyes": ":heart_eyes:",
@@ -9,28 +8,31 @@ emoji_dict = {
     "rocket": ":rocket:",
 }
 
+
 def send_message_with_emoji(sender, receiver, message):
     formatted_message = message
     for alias, emoji_code in emoji_dict.items():
-        formatted_message = formatted_message.replace(alias, emoji.emojize(emoji_code, use_aliases=True))
+        formatted_message = formatted_message.replace(
+            alias, emoji.emojize(emoji_code, use_aliases=True))
     print(f"{sender}: {formatted_message}")
     return formatted_message
+
 
 def test_send_message_with_emoji():
     sender = "NYU"
     receiver = "Tandon"
     message = "You are :heart_eyes: and :fire: :thumbs_up: :rocket:"
     expected_message = "You are 😍 and 🔥 👍 🚀"
-    
+
     result = send_message_with_emoji(sender, receiver, message)
     assert result == expected_message, f"Expected: {expected_message}, but got: {result}"
+
 
 if __name__ == "__main__":
     sender = input("Username: ")
     receiver = input("Recipient: ")
     message = input("Your text here: ")
     formatted_message = send_message_with_emoji(sender, receiver, message)
-    print(f"{formatted_message}")
-    
-    # Run the test
+    print(formatted_message)
+
     test_send_message_with_emoji()
