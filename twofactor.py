cd8ac7e2fb36a4301e716b98428aad9e999f608c
pablohop <71025955+pablohop@users.noreply.github.com>
2023-11-18 14:51:24 -0500

    Update twofactor.py
    Rewrote according to flake8 standards

--- twofactor.py
+++ twofactor.py
@@ -1,6 +1,9 @@
+import random
+
 user_data = {}
 two_factor_auth = {}
 two_factor_codes = {}
+
 
 def reset_password(username, new_password):
     if username in user_data:
@@ -9,55 +12,53 @@ def reset_password(username, new_password):
     else:
         print("Unable to find user")
 
+
 def verify_login(username, password):
-    if username in user_data and user_data[username] == password:
-        return True
-    return False
-    
+    return username in user_data and user_data[username] == password
+
+
 def enable_2fa(username):
     if username in user_data:
         two_factor_auth[username] = True
-        print(f"Two-Factor Authentication enabled")
+        print("Two-Factor Authentication enabled")
     else:
         print("Error")
-        
+
+
 def generate_2fa_code(username):
-    import random
     two_factor_codes[username] = str(random.randint(1000, 9999))
 
+
 def perform_2fa(username):
-    if username in user_data and username in two_factor_auth and two_factor_auth[username]:
-        phone_number = user_data[username]["phone_number"]
-        #the function will simulate sending a code to a phone number
-        #i will figure out how to do this more later
+    if username in user_data and two_factor_auth.get(username, False):
         generate_2fa_code(username)
         input_code = input("Code: ")
         stored_code = two_factor_codes.get(username, "")
-        
+
         if input_code == stored_code:
             print("Authentication Complete")
             return True
         else:
             print("Incorrect Code")
-    
+
     return False
+
 
 def test_reset_password():
     username = "swe123"
     old_password = "password"
     new_password = "Betterpassword456!"
-    
+
     user_data[username] = old_password
     reset_password(username, new_password)
-    
-    assert verify_login(username, new_password) == True, "Password reset failed"
+
+    assert verify_login(username, new_password), "Password reset failed"
+
 
 if __name__ == "__main__":
     username = input("Username: ")
     password = input("Password: ")
     if verify_login(username, password):
-        #i know this line doesn't make sense after already entering your
-        #password but the above were for test purposes
         reset_choice = input("Forgot your password?").lower()
         if reset_choice == "yes":
             if perform_2fa(username):
