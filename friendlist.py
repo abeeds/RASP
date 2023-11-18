cc5d013563c70087dd4c376425b73ed31c8092f1
pablohop <71025955+pablohop@users.noreply.github.com>
2023-11-18 14:57:30 -0500

    Update friendlist.py
    Rewrote according to flake8 standards

--- friendlist.py
+++ friendlist.py
@@ -4,6 +4,7 @@ user_contacts = {
     "chance338": ["johnny215", "rosa413"],
 }
 
+
 def display_contacts(username):
     if username in user_contacts:
         contacts = user_contacts[username]
@@ -15,6 +16,7 @@ def display_contacts(username):
     else:
         print("No profile match.")
 
+
 def add_contact(username, new_contact):
     if username in user_contacts:
         if new_contact not in user_contacts[username]:
@@ -25,18 +27,19 @@ def add_contact(username, new_contact):
     else:
         print("No profile match.")
 
+
 if __name__ == "__main__":
     while True:
-        choice = input("Would you like to view your friends, add a new friend or exit?").strip().lower()
+        choice = input("Would you like to view your friends, add a new friend or exit? ").lower()
         
-        if choice == "view" or choice == "View":
+        if choice == "view":
             username = input("Please enter username: ")
             display_contacts(username)
-        elif choice == "add" or choice == "Add":
+        elif choice == "add":
             username = input("Please enter your username: ")
             new_contact = input("Please enter the friend username: ")
             add_contact(username, new_contact)
-        elif choice == "exit" or choice == "Exit":
+        elif choice == "exit":
             print("Logging Off")
             break
         else:
