29ab98e664cef4612088d8e538f4d77acc786799
pablohop <71025955+pablohop@users.noreply.github.com>
2023-11-18 14:48:48 -0500

    Update textsearch.py
    Rewrote according to flake8 standards

--- textsearch.py
+++ textsearch.py
@@ -1,17 +1,19 @@
 def search_and_highlight(text, keyword):
-    highlighted_text = text.replace(keyword, f"\033[1;31;47m{keyword}\033[m")
+    highlighted_text = text.replace(keyword, f"\\033[1;31;47m{keyword}\\033[m")
     return highlighted_text
+
 
 def test_search_and_highlight():
     text = "Please pick up eggs and milk at the grocery store."
     keyword = "grocery"
-    result = search_and_highlight(text, keyword)
+    search_and_highlight(text, keyword)  # The result is not used
+
 
 if __name__ == "__main__":
     text = "Hello! Reminder that the meeting is at 7 pm."
     keyword = input("What are you searching for? ")
-    highlighted_text = search_and_hihlight(text, keyword)
-    print("Results: ")
+    highlighted_text = search_and_highlight(text, keyword)
+    print("Results:")
     print(highlighted_text)
     test_search_and_highlight()
     print("Finished")
