0e4e2802790b6c1dcfe91a458e925c92e40ec005
Shahria Abeed <shahriaabeed@gmail.com>
2023-11-30 12:50:11 -0500

    added insert user functions

--- db/db_users.py
+++ db/db_users.py
@@ -1,7 +1,24 @@
-# from .db_connect import insert_one, USER_DB
+from .db_connect import insert_one, connect_db
+from .db_connect import fetch_one
 
-USER_COLLECTION = "users"
+USER_COLLECT = "users"
 
 
-def insert_user(username, password):
-    pass
+def user_exists(username: str):
+    connect_db()
+    return fetch_one(USER_COLLECT, {"Username": username})
+
+
+def insert_user(username: str, password: str):
+    if user_exists(username):
+        raise ValueError(f'Username already exists: {username}')
+    if not username:
+        raise ValueError('Username cannot be blank')
+
+    user = {}
+    user["Username"] = username
+    user["password"] = password
+
+    connect_db()
+    _id = insert_one(USER_COLLECT, user)
+    return _id is not None
