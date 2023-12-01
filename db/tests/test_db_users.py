fcdf28986147c62e4c9077e7c69ff812db7c44a6
Shahria Abeed <shahriaabeed@gmail.com>
2023-11-30 22:41:53 -0500

    added test_update_password

--- db/tests/test_db_users.py
+++ db/tests/test_db_users.py
@@ -3,6 +3,7 @@ import db.db_users as dbu
 
 TEST_USERNAME = "test"
 TEST_PASSWORD = "testpw"
+TEST_NEWPW = "updatedpw"
 
 
 @pytest.fixture(scope='function')
@@ -18,6 +19,15 @@ def temp_user():
 def test_insert_user():
     dbu.insert_user(TEST_USERNAME, TEST_PASSWORD)
     assert dbu.user_exists(TEST_USERNAME)
+
+
+def test_update_password():
+    dbu.update_password(TEST_USERNAME, TEST_NEWPW)
+    user = dbu.user_exists(TEST_USERNAME)
+
+    assert user is not None
+    assert user["Password"] != TEST_PASSWORD
+    assert user["Password"] == TEST_NEWPW
 
 
 def test_deactivate():
