515bf47f224c18b47fceffb5fa3ff387c1d4707d
Shahria Abeed <shahriaabeed@gmail.com>
2023-12-11 11:15:44 -0500

    added test for delete_message

--- db/tests/test_db_messages.py
+++ db/tests/test_db_messages.py
@@ -4,6 +4,7 @@ import db.db_messages as dbm
 TEST_USERNAME = "First Last"
 TEST_CHATROOM = "test"
 TEST_CONTENT = "Lorem ipsum dolor sit amet"
+TEST_ID = ""  # set later
 
 
 @pytest.fixture(scope='function')
@@ -14,5 +15,15 @@ def temp_message():
 
 
 def test_insert_message():
+    global TEST_ID
+
     msgres = dbm.insert_message(TEST_USERNAME, TEST_CHATROOM, TEST_CONTENT)
-    assert dbm.message_exists(msgres[0])
+    TEST_ID = msgres[0]
+    assert dbm.message_exists(TEST_ID)
+
+
+def test_delete_message():
+    global TEST_ID
+
+    dbm.delete_message(TEST_ID)
+    assert not dbm.message_exists(TEST_ID)
