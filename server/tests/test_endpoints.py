432ea4586d726ca608a52a8905a8af5f046e2ee7
Shahria Abeed <shahriaabeed@gmail.com>
2023-12-11 23:41:01 -0500

    deleted unused tests

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -18,27 +18,3 @@ def test_hello():
     resp_json = resp.get_json()
     print(f'{resp_json=}')
     assert ep.HELLO_RESP in resp_json
-
-
-def test_list_users():
-    resp = TEST_CLIENT.get(ep.USERS_EP)
-    assert resp.status_code == OK
-    resp_json = resp.get_json()
-    assert isinstance(resp_json, dict)
-    assert ep.TITLE in resp_json
-    assert ep.TYPE in resp_json
-    assert ep.DATA in resp_json
-
-
-@patch('db.db_users.insert_user', side_effect=ValueError(), autospec=True)
-def test_users_bad_add(mock_add):
-    resp = TEST_CLIENT.post(ep.USERS_EP, json=
-        {"username": "test username", "password": "test password"})
-    assert resp.status_code == NOT_ACCEPTABLE
-
-
-@patch('db.db_users.insert_user', return_value=usrs.MOCK_ID, autospec=True)
-def test_users_add(mock_add):
-    resp = TEST_CLIENT.post(ep.USERS_EP, json=
-        {"username": "test username", "password": "test password"})
-    assert resp.status_code == OK
