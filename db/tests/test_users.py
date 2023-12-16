f429a20d749623e6db19c525a195ec5abd687080
thejollyfatso <rafael.r.deleon@gmail.com>
2023-12-16 01:19:02 -0800

    skipped tests from users.py as db_users.py exists

--- db/tests/test_users.py
+++ db/tests/test_users.py
@@ -12,23 +12,26 @@ def temp_user():
         usrs.del_user(name)
 
 
+@pytest.mark.skip('parent file defunct')
 def test_get_test_name():
     name = usrs._get_test_name()
     assert isinstance(name, str)
     assert len(name) > 0
 
 
+@pytest.mark.skip('parent file defunct')
 def test_gen_id():
     _id = usrs._gen_id()
     assert isinstance(_id, str)
     assert len(_id) == usrs.ID_LEN
 
 
+@pytest.mark.skip('parent file defunct')
 def test_get_test_user():
     assert isinstance(usrs.get_test_user(), dict)
 
 
-@pytest.mark.skip('skip unnecessary test')
+@pytest.mark.skip('parent file defunct')
 def test_get_users(temp_user):
     users = usrs.get_users()
     assert isinstance(users, dict)
@@ -37,11 +40,13 @@ def test_get_users(temp_user):
             assert isinstance(user, str)
 
 
+@pytest.mark.skip('parent file defunct')
 def test_get_unread_from():
     messages = usrs.get_unread_from(usrs.TEST_USER)
     assert isinstance(messages, dict)
 
 
+@pytest.mark.skip('parent file defunct')
 def test_add_user_blank_name():
     with pytest.raises(ValueError):
         usrs.add_user('')
@@ -50,20 +55,20 @@ def test_add_user_blank_name():
 ADD_NAME = "First Last"
 
 
-@pytest.mark.skip('Must be updated for Mongo (currently breaks build.)')
+@pytest.mark.skip('parent file defunct')
 def test_add_user():
     usrs.add_user(ADD_NAME)
     assert usrs.exists(ADD_NAME)
 
 
-@pytest.mark.skip('Must be updated for Mongo (Even though still passes.)')
+@pytest.mark.skip('parent file defunct')
 def test_del_user(temp_user):
     name = temp_user
     usrs.del_user(name)
     assert not usrs.exists(name)
 
 
-@pytest.mark.skip('Must be updated for Mongo (Even though still passes.)')
+@pytest.mark.skip('parent file defunct')
 def test_del_nonexistent_user():
     name = frozenset(usrs.get_test_user())
     with pytest.raises(ValueError):
