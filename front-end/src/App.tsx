060db4715748edffcb56d5edb402eb0a07715925
Shahria Abeed <shahriaabeed@gmail.com>
2024-02-01 12:12:19 -0500

    added commented imports for future reference

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react' 
+// import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// import {BrowserRouter,Routes,Route} from 'react-router-dom';
+// import {BrowserRouter,Routes,Route} from 'react-router-dom'
+// import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
