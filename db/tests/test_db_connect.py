f44b9223d55b292bc6ce81613590b564e58478f5
thejollyfatso <rafael.r.deleon@gmail.com>
2024-03-28 12:13:02 -0700

    added del_all_in_collection test

--- db/tests/test_db_connect.py
+++ db/tests/test_db_connect.py
@@ -4,9 +4,15 @@ import db.db_connect as dbc
 
 TEST_DB = dbc.USER_DB
 TEST_COLLECT = 'test_collect'
+TEST_COLLECT_MANY = 'test_collect_many'
 # can be used for field and value:
 TEST_NAME = 'test'
 UPDATE = 'Update'
+SAMPLE_DOCS = [
+    {TEST_NAME: TEST_NAME},
+    {TEST_NAME: TEST_NAME},
+    {TEST_NAME: TEST_NAME}
+]
 
 
 @pytest.fixture(scope='function')
@@ -16,6 +22,14 @@ def temp_rec():
     # yield to our test function
     yield
     dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
+
+
+@pytest.fixture(scope='function')
+def temp_collection():
+    dbc.connect_db()
+    dbc.client[TEST_DB][TEST_COLLECT_MANY].insert_many(SAMPLE_DOCS)
+    yield dbc.client[TEST_DB][TEST_COLLECT_MANY]
+    dbc.client[TEST_DB].drop_collection(TEST_COLLECT_MANY)
 
 
 def test_fetch_one(temp_rec):
@@ -32,3 +46,9 @@ def test_update_doc(temp_rec):
     dbc.update_doc(TEST_COLLECT, {TEST_NAME: TEST_NAME}, {TEST_NAME: UPDATE})
     ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: UPDATE})
     assert ret is not None
+
+
+def test_del_all_in_collection(temp_collection):
+    assert temp_collection.count_documents({}) == 3
+    dbc.del_all_in_collection(TEST_COLLECT_MANY)
+    assert temp_collection.count_documents({}) == 0
