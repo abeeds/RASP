bc6b690ee0b57cbc18341a1dbbb9e6d767f3ee56
Shahria Abeed <105658920+abeeds@users.noreply.github.com>
2024-04-08 12:13:11 -0400

    another possible fix

--- db/db_messages.py
+++ db/db_messages.py
@@ -47,7 +47,7 @@ def get_chatroom_messages(chatroom: str, pages="ALL"):
         pages = int(pages)
         messages = fetch_many(MESSAGE_COLLECT,
                               {CHATROOM: chatroom},
-                              10 * pages)
+                              pages)
 
     message_dict = {str(msg["_id"]):
                     {
