713192aaf3e36e6ad9c5b1a9a5624a3f3971af13
abeeds <shahriaabeed@gmail.com>
2024-04-23 10:48:00 -0400

    typo

--- db/tests/test_db_messages.py
+++ db/tests/test_db_messages.py
@@ -27,9 +27,9 @@ def test_insert_message():
 
 
 def test_edit_message():
-    dbm.edit_message(TEST_ID)
+    dbm.edit_message(TEST_ID, TEST_NEW_CONTENT)
 
-    msg = dbm.message_exists(TEST_ID, TEST_NEW_CONTENT)
+    msg = dbm.message_exists(TEST_ID)
     assert msg[dbm.CONTENT] == TEST_NEW_CONTENT
 
 
